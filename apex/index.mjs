37aced26368ebf6e5d37fbf393bc01486a4d746a
jpdoshi <jpdoshi2811@gmail.com>
2024-10-20 12:37:10 +0530

    compiler: count execution time

--- apex/index.mjs
+++ apex/index.mjs
@@ -3,13 +3,18 @@ import { bundleModules } from "./bundler.mjs";
 import cliUtils from "./cli.mjs";
 
 const build = async (entryPoint) => {
+  const startTime = Date.now();
+
   cliUtils.info("STEP-1: COMPILING MODULES");
   await compileModule(entryPoint);
 
   cliUtils.info("\nSTEP-3: BUNDLING MODULES");
   await bundleModules();
 
-  cliUtils.warn("\nInfo: Execution Complete");
+  const endTime = Date.now();
+  const executionTime = endTime - startTime;
+
+  cliUtils.warn(`\nInfo: Compilation Complete (${executionTime}ms)`);
 };
 
 const Apex = {
